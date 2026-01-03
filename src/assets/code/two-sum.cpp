874cec1b441844c5f7c240274006092c6e394328
g00d-kid-10 <shubhampr10@gmail.com>
2026-01-03 22:41:12 +0530

    Two sum refactor

--- src/assets/code/two-sum.cpp
+++ src/assets/code/two-sum.cpp
@@ -1,13 +1,12 @@
-vector<int> two_sum(vector<int>& nums, int target_sum) {
+vector<int> two_sum(vector<int>& nums, int target) {
     int n = nums.size();
     unordered_map<int, int> mp;
-    for(int i = 0; i < n; i++) mp[nums[i]] = i;
-    
     for(int i = 0; i < n; i++) {
-        int target = target_sum - nums[i]; 
-        if(mp.count(target) && mp[target] != i) {
-            return {i, mp[target]};
+        int need = target - nums[i];
+        if(mp.count(need)) {
+            return {mp[need], i};
         }
+        mp[nums[i]] = i;
     }
 
     return {-1, -1};
