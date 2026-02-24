31e7b14c8ffc89f8b66490995ef99b9253048ab6
g00d-kid-10 <shubhampr10@gmail.com>
2026-02-24 09:08:04 +0530

    rotate array updateas

--- src/assets/code/rotate-array-iterative.cpp
+++ src/assets/code/rotate-array-iterative.cpp
@@ -1,8 +1,12 @@
 void rotate_array_iterative(vector<int>& nums, int k) {
     int n = nums.size();
+    k %= n;
     vector<int> ans(n);
     for(int i = 0; i < n; i++) {
-        ans[(i + k) % n] = nums[i];
+        // i -> reps left
+        // (i + K) % n -> reps right
+        ans[(i + k) % n] = nums[i]; // right -> pushing from left(i) to right((i + k) % n).
+        nums[i] = ans[(i + k) % n]; // left -> pulling from right to left.
     }
     nums = ans;
 }
