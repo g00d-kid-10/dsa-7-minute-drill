13640577fdcd155d7b2ef24db23a9c9b646335f8
g00d-kid-10 <shubhampr10@gmail.com>
2026-02-27 20:00:39 +0530

    [FIX] - pivot

--- src/assets/code/pivot.cpp
+++ src/assets/code/pivot.cpp
@@ -1,19 +1,18 @@
 int pivot(vector<int>& nums) {
     int n = nums.size();
-    int l = 0, r = n - 1, ans = -1;
-    while(l <= r) {
-        if(nums[l] <= nums[r]) {
-            ans = l;
-            break;
-        }
-        
+    int l = 0, r = n - 1;
+
+    // Only if duplicates present (TC: O(n))
+    while(l < n - 1 && nums[l] == nums[l + 1]) l++;
+    while(r > 0 && nums[r - 1] == nums[r]) r--;
+    
+    while(l < r) {
         int m = l + (r - l) / 2;
-        if(nums[l] <= nums[m]) {
+        if(nums[m] > nums[r]) {
             l = m + 1;
         } else {
             r = m;
         }
     }
-
-    return ans;
+    return l;
 }
