cc33258891fc64c51c8544860ed0d886216a45ea
g00d-kid-10 <shubhampr10@gmail.com>
2026-02-27 20:04:50 +0530

    [FIX] - rotated search

--- src/assets/code/rotated-search.cpp
+++ src/assets/code/rotated-search.cpp
@@ -1,5 +1,6 @@
-int rotated_search(vector<int>& nums, int val) {
-    // find pivot idx
-    // search(0, p_idx);
+  int rotated_search(vector<int>& nums, int val) {
+    // find pivot idx 
+    // Implementation of pivot depends on weather duplicates are present
+    // search(0, p_idx - 1);
     // search(p_idx, n - 1);
 }
