c6212a63e5619c134037c829b05c89857916b20d
g00d-kid-10 <shubhampr10@gmail.com>
2026-03-11 13:01:28 +0530

    tree node path

---
+++ src/assets/code/tree-node-path.cpp
@@ -0,0 +1,15 @@
+bool tree_node_path(TreeNode* root, TreeNode* target, vector<TreeNode*>& ans) {
+    if(!root) return false;
+    ans.push_back(root);
+    if(root == target) {
+        return true;
+    }
+
+    if(path(root->left, target, ans) || path(root->right, target, ans)) {
+        return true;
+    }
+    
+    ans.pop_back();
+
+    return false;
+}
