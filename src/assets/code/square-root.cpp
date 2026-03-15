2898a1baa82c911b84859a29c68e51773d4ece19
g00d-kid-10 <shubhampr10@gmail.com>
2026-03-15 17:22:25 +0530

    square root

---
+++ src/assets/code/square-root.cpp
@@ -0,0 +1,14 @@
+int square_root(int num) {
+    if(num <= 1) return num;
+    int l = 0, r = num, ans = -1;
+    while(l <= r) {
+        int m = l + (r - l) / 2;
+        if(m <= num / m) { // Instead of m * m; Please don't forget this division overflow prevention trick. Please! Please! Please!
+            ans = m;
+            l = m + 1;
+        } else {
+            r = m - 1;
+        }
+    }
+    return ans;
+}
