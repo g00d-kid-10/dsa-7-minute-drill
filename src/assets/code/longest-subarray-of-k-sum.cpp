74ec093fb8026bff93ee1ff3bc099318a6c603f8
g00d-kid-10 <shubhampr10@gmail.com>
2026-03-15 15:57:20 +0530

    Longest subarray with k sum

---
+++ src/assets/code/longest-subarray-of-k-sum.cpp
@@ -0,0 +1,15 @@
+int longest_subarray_of_k_sum(vector<int>& nums, int k) {
+    int n = nums.size();
+    int sum = 0, ans = 0;
+    for(int i = 0, j = 0; j < n; j++) {
+        sum += nums[j];
+        while(i <= j && sum > k) {
+            sum -= nums[i++];
+        }
+        
+        if(sum == k) { // Remove this condition for at least k sum
+            ans = max(ans, j - i + 1);
+        }
+    }
+    return ans;
+}
