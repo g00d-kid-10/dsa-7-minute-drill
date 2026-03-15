63901de80d4c61a69664b6e90a09fbab350fe259
g00d-kid-10 <shubhampr10@gmail.com>
2026-03-15 17:51:19 +0530

    updates

--- src/assets/code/remove-duplicate-from-sorted.cpp
+++ src/assets/code/remove-duplicate-from-sorted.cpp
@@ -1,8 +1,7 @@
 int remove_duplicate_from_sorted(vector<int>& nums) {
     int n = nums.size();
     if(n == 0) return 0;
-    int i = 0;
-    for(int j = 0; j < n; j++) {
+    for(int i = 0, int j = 0; j < n; j++) {
         if(nums[i] == nums[j]) continue;
         i++;
         nums[i] = nums[j];
--- src/assets/code/reverse-bits.cpp
+++ src/assets/code/reverse-bits.cpp
@@ -1,3 +1,7 @@
+// Complete reversal of 32 Bits Integer
+// 43261596	    00000010100101000001111010011100
+// 964176192	00111001011110000010100101000000
+
 int reverse_bits(int n) {
     int ans = 0;
     for(int i = 31; i > -1; i--) {
