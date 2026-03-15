83e46ae0192af58db3d5635e16c76dbfc353002e
g00d-kid-10 <shubhampr10@gmail.com>
2026-03-15 21:15:28 +0530

    count subarray sum equals k

--- src/assets/code/count-subarray-sum-equals-k.cpp
+++ src/assets/code/count-subarray-sum-equals-k.cpp
@@ -1,11 +1,11 @@
 // PREFIX SUM METHOD
-int subarray_sum_equals_k(vector<int>& nums, int k) {
+int count_subarray_sum_equals_k(vector<int>& nums, int k) {
     int n = nums.size();
     vector<int> pre_sum(n + 1);
     for(int i = 1; i < n + 1; i++) pre_sum[i] = pre_sum[i - 1] + nums[i - 1];
     
     int ans = 0;
-    for(int i = 0; i > n; i++) {
+    for(int i = 0; i < n; i++) {
         for(int j = i + 1; j < n + 1; j++) {
             int sum = pre_sum[j] - pre_sum[i];
             sum == k ? ans++ : ans;
@@ -17,8 +17,8 @@ int subarray_sum_equals_k(vector<int>& nums, int k) {
 
 // HASMMAP METHOD => Keep track of frequencies of sum occured and on each iteration look for how many compilments(sum - k) already existed
 // because FROM all those indexes where this complement occured TO this current itteration index all((i.e. total no of frequency)) will form a subarray of sum k.
-// Current iteration sum - previously occured compliment = k 
-int subarray_sum_equals_k(vector<int>& nums, int k) {
+// Current iteration sum - previously occured compliment = k
+int count_subarray_sum_equals_k(vector<int>& nums, int k) {
     int n = nums.size();
     unordered_map<int, int> mp;
     mp[0] = 1;
