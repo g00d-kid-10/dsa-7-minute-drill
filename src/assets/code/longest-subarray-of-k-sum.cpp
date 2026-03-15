ed5af77eb67b6bdaeb63f4b7f4c80b05d8965589
g00d-kid-10 <shubhampr10@gmail.com>
2026-03-15 23:52:02 +0530

    Longest subarray of K sum

--- src/assets/code/longest-subarray-of-k-sum.cpp
+++ src/assets/code/longest-subarray-of-k-sum.cpp
@@ -1,3 +1,22 @@
+// HASMAP => For integers
+int longest_subarray_of_k_sum(vector<int>& nums, int k) {
+    int n = nums.size();
+    int sum = 0, ans = 0;
+    unordered_map<int, int> mp; 
+    mp[0] = -1;
+    for(int i = 0; i < n; i++) {
+        sum += nums[i];
+        if(mp.count(sum - k)) {
+            ans = max(ans, i - mp[sum - k]);
+        }
+        if(!mp.count(sum)) { // First occurence of idx
+            mp[sum] = i;
+        }
+    }
+    return ans;
+}
+
+// SLIDING WINDOW => For positive numbers only
 int longest_subarray_of_k_sum(vector<int>& nums, int k) {
     int n = nums.size();
     int sum = 0, ans = 0;
