93eaf63a2fc5a448a51bdbea5c3b8a175501cea8
g00d-kid-10 <shubhampr10@gmail.com>
2026-03-16 12:12:51 +0530

    updates

--- src/assets/code/minimum-path-sum.cpp
+++ src/assets/code/minimum-path-sum.cpp
@@ -1,6 +1,5 @@
 int minimum_path_sum(vector<vector<int>>& grid) {
-    int m = grid.size();
-    int n = grid[0].size();
+    int m = grid.size(), n = grid[0].size();
     vector<pair<int, int>> dirs = {{0, 1}, {1, 0}};
     vector<vector<int>> cache(m, vector<int>(n, -1));
     
@@ -8,14 +7,13 @@ int minimum_path_sum(vector<vector<int>>& grid) {
         if(r == m || c == n) return 1e9;
         if(r == m - 1 && c == n - 1) return grid[r][c];
         if(cache[r][c] != -1) return cache[r][c];
-
-        int ans = 1e9;
-        for(pair<int, int> dir : dirs) {
-            int sub_ans = dfs(r + dir.first, c + dir.second);
-            ans = min(ans, grid[r][c] + sub_ans);
+    
+        int minimum = 1e9;
+        for(auto& [dr, dc] : dirs) {
+            minimum = min(minimum, dfs(r + dr, c + dc));
         }
 
-        return cache[r][c] = ans;
+        return cache[r][c] = grid[r][c] + minimum;
     };
     
     return dfs(0, 0);
